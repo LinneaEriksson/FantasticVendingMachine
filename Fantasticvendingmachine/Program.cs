07388b1cd317c7f1652813adc9febf6501a42fd5
amandaprintz <fannyamandakarlsson@gmail.com>
2022-03-17 13:43:02 +0100

    Update Program.cs

--- Fantasticvendingmachine/Program.cs
+++ Fantasticvendingmachine/Program.cs
@@ -1,5 +1,17 @@
 ﻿using Fantasticvendingmachine;
 
+var bank = new Bank();
+var user = new User();
+{
+    Name = "Snusmumriken";
+    Money = 200; 
+};
+
+var vendingmachine = new VendingMachine();
+{
+    
+};
+
 Console.WriteLine("Hello and welcome to this Fantastic vending machine!");
 
 Console.WriteLine("What would you like to buy?");
