41c100cfd45c1e24163b4a3be289ef9e18b473ca
NellySP <nelly.petren@gmail.com>
2022-03-17 14:12:08 +0100

    get that list

--- Fantasticvendingmachine/Program.cs
+++ Fantasticvendingmachine/Program.cs
@@ -1,11 +1,8 @@
 ﻿using Fantasticvendingmachine;
 
 var bank = new Bank();
-var user = new User();
-{
-    Name = "Snusmumriken";
-    Money = 200; 
-};
+var user = new User("Snusmumriken", 150);
+
 
 var vendingmachine = new VendingMachine();
 {
@@ -13,7 +10,12 @@ var vendingmachine = new VendingMachine();
 };
 
 Console.WriteLine("Hello and welcome to this Fantastic vending machine!");
-
+Console.WriteLine("This is what we currently have!");
+ foreach (var item in vendingmachine.inventory.items)
+{
+    Console.WriteLine(item.Name + ": " + item.Price + ":-");
+  
+}
 Console.WriteLine("What would you like to buy?");
 Console.WriteLine();
 Console.WriteLine("");
